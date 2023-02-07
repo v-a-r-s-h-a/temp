8d3933f388bd890c90e2de0aee51a2696b52da6e
v-a-r-s-h-a <59343612+v-a-r-s-h-a@users.noreply.github.com>
2023-02-07 21:51:06 +0530

    Add files via upload

---
+++ Logout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Logout = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>This is Logout</h1>
+      </div>
+    </>
+  );
+};
+
+export default Logout;
